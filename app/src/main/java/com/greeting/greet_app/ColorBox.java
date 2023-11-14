50e8ec8e2ca827dcacdb11914102e58850f85d0a
devetech <girishg.etechmavens@gmail.com>
2023-11-14 17:55:32 +0530

    1.Color Changes is Done 2.Gradiant Color Picker is Pending Right There So lets Come With these

--- app/src/main/java/com/greeting/greet_app/ColorBox.java
+++ app/src/main/java/com/greeting/greet_app/ColorBox.java
@@ -3,16 +3,22 @@ package com.greeting.greet_app;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.GradientDrawable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.cardview.widget.CardView;
 
 import com.madrapps.pikolo.RGBColorPicker;
 import com.madrapps.pikolo.listeners.SimpleColorSelectionListener;
+import com.skydoves.colorpickerview.ColorEnvelope;
+import com.skydoves.colorpickerview.ColorPickerDialog;
+import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener;
 
 public class ColorBox {
 
@@ -20,10 +26,11 @@ public class ColorBox {
     GradientDrawable drawable;
     Button btnCencel, btnDone;
     int[] colors;
+    TextView txtfirst,txtsecond;
     View view;
 
     CardView testBgColor;
-    RGBColorPicker firstColor, SecondColor;
+    CardView firstColor, SecondColor;
     Activity activity;
     Dialog dialog;
 
@@ -42,35 +49,62 @@ public class ColorBox {
         }
         dialog = new Dialog(activity);
         dialog.setCancelable(false);
-        //dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        dialog.setTitle("Choose Gradiant Color");
-        dialog.setContentView(R.layout.color_picker);
-        testBgColor = dialog.findViewById(R.id.testBgColor);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        View v = activity.getLayoutInflater().inflate(R.layout.color_picker, null);
+        dialog.setContentView(v);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        txtfirst = dialog.findViewById(R.id.textview_first);
+        txtsecond = dialog.findViewById(R.id.textview_second);
+        testBgColor = dialog.findViewById(R.id.outPutTest);
         firstColor = dialog.findViewById(R.id.firstColor);
-        SecondColor = dialog.findViewById(R.id.secondColor);
+        SecondColor = dialog.findViewById(R.id.SecondColor);
         btnCencel = dialog.findViewById(R.id.btnCencel);
         btnDone = dialog.findViewById(R.id.btnDone);
         colors = new int[]{1, 2};
         drawable = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, colors);
-        firstColor.setColorSelectionListener(new SimpleColorSelectionListener() {
-            @Override
-            public void onColorSelected(int color) {
-                super.onColorSelected(color);
-                colors[0] = color;
-                drawable.setColors(colors);
-                testBgColor.setBackground(drawable);
-                listner.setDrawable(colors);
-            }
+        firstColor.setOnClickListener(view1 -> {
+            new ColorPickerDialog.Builder(activity)
+                    .setTitle("Select First Gradiant")
+                    .setPreferenceName("MyColorPickerDialog")
+                    .setPositiveButton("OK", new ColorEnvelopeListener() {
+                        @Override
+                        public void onColorSelected(ColorEnvelope envelope, boolean fromUser) {
+                            colors[0] = envelope.getColor();
+                            drawable.setColors(colors);
+                            testBgColor.setBackground(drawable);
+                            txtfirst.setText(convertColorToHex(envelope.getColor()));
+                            firstColor.setCardBackgroundColor(envelope.getColor());
+                        }
+                    })
+                    .setNegativeButton("CENCEL",(dialogInterface, i) -> {
+                        dialogInterface.dismiss();
+                    })
+                    .attachBrightnessSlideBar(true)
+                    .setBottomSpace(12)
+                    .attachAlphaSlideBar(true)
+                    .show();
         });
-        SecondColor.setColorSelectionListener(new SimpleColorSelectionListener() {
-            @Override
-            public void onColorSelected(int color) {
-                super.onColorSelected(color);
-                colors[1] = color;
-                drawable.setColors(colors);
-                listner.setDrawable(colors);
-                testBgColor.setBackground(drawable);
-            }
+        SecondColor.setOnClickListener(view1 -> {
+            new ColorPickerDialog.Builder(activity)
+                    .setTitle("Select First Gradiant")
+                    .setPreferenceName("MyColorPickerDialog")
+                    .setPositiveButton("OK", new ColorEnvelopeListener() {
+                        @Override
+                        public void onColorSelected(ColorEnvelope envelope, boolean fromUser) {
+                            colors[1]=envelope.getColor();
+                            drawable.setColors(colors);
+                            txtsecond.setText(convertColorToHex(envelope.getColor()));
+                            testBgColor.setBackground(drawable);
+                            SecondColor.setCardBackgroundColor(envelope.getColor());
+                        }
+                    })
+                    .setNegativeButton("CENCEL",(dialogInterface, i) -> {
+                        dialogInterface.dismiss();
+                    })
+                    .attachBrightnessSlideBar(true)
+                    .setBottomSpace(12)
+                    .attachAlphaSlideBar(true)
+                    .show();
         });
         btnCencel.setOnClickListener(view1 -> {
             dialog.dismiss();
@@ -78,6 +112,8 @@ public class ColorBox {
         btnDone.setOnClickListener(view1 -> {
             listner.setDrawable(colors);
             dialog.dismiss();
+            listner.setDrawable(colors);
+
         });
         dialog.show();
     }
@@ -97,6 +133,9 @@ public class ColorBox {
         return false;
     }
 
+    public String convertColorToHex(int color) {
+        return String.format("#%06X", 0xFFFFFF & color);
+    }
 }
 
 interface ColorListner {
